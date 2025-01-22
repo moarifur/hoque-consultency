ed15ab61d6d3e190a6a1c3056a3375ff20c798de
moarifur <moarifur@gmail.com>
2025-01-22 20:40:23 +0600

    Deployment error solving next/font error:

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["Poppins"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["Poppins"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Hoque Consultancy",
@@ -21,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={inter.className}>
         <Navbar/>
         <div className="mt-16">
             {children}
